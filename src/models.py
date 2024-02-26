b2f186079d8bb46e62164037ce3d80335c994446
Hal <hal0dickinson@gmail.com>
2024-02-26 09:39:32 +0000

    Added basic Flask and user login

---
+++ src/models.py
@@ -0,0 +1,10 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    password = db.Column(db.String(100))
+    username = db.Column(db.String(100))
+    userType = db.Column(db.String(100))
+
+
